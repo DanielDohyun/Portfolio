a76dc821c7f00a355a593f3f60e7c1faf74d91f0
Daniel Kim <kkdh1109@gmail.com>
2020-12-17 13:03:54 -0500

    inital folder setup and create header, footer comp

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Daniel Kim</h1>
+            <div className="header__right">
+                <p>ABOUT</p>
+                <P>WORK</P>
+                <P>CONTACT</P>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
