9c21a6f0b2d17ee3e0f8a487d97af7afd6bd1cdb
Daniel Kim <kkdh1109@gmail.com>
2021-01-05 14:50:16 -0500

    basic grid structure for cards

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -7,50 +7,55 @@ import closet from '../../imgs/closet.png';
 function Work() {
     return (
         <div id="work" className="work">
-            <Card className="work__card" style={{ width: '18rem' }}>
-                <Card.Img className="work__cardImg" variant="top" src={closet} />
-                <Card.Body>
-                    <Card.Title className="work__cardTitle">Closet Organizer</Card.Title>
-                    <Card.Text className="work__cardDesc">
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    </Card.Text>
-                    <Button className="work__cardBtn" variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
-            <Card className="work__card" style={{ width: '18rem' }}>
-                <Card.Img className="work__cardImg" variant="top" src={closet} />
-                <Card.Body>
-                    <Card.Title className="work__cardTitle">Closet Organizer</Card.Title>
-                    <Card.Text className="work__cardDesc">
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    </Card.Text>
-                    <Button className="work__cardBtn" variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
-            <Card className="work__card" style={{ width: '18rem' }}>
-                <Card.Img className="work__cardImg" variant="top" src={closet} />
-                <Card.Body>
-                    <Card.Title className="work__cardTitle">Closet Organizer</Card.Title>
-                    <Card.Text className="work__cardDesc">
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    </Card.Text>
-                    <Button className="work__cardBtn" variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
-            <Card className="work__card" style={{ width: '18rem' }}>
-                <Card.Img className="work__cardImg" variant="top" src={closet} />
-                <Card.Body>
-                    <Card.Title className="work__cardTitle">Closet Organizer</Card.Title>
-                    <Card.Text className="work__cardDesc">
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    </Card.Text>
-                    <Button className="work__cardBtn" variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
+                <h1>React App</h1>
+            <div className="work__react">
+                <Card className="work__card" >
+                    <Card.Img className="work__cardImg" variant="top" src={closet} />
+                    <Card.Body>
+                        <Card.Title className="work__cardTitle">Closet Organizer</Card.Title>
+                        <Card.Text className="work__cardDesc">
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button className="work__cardBtn" variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+                <Card className="work__card" >
+                    <Card.Img className="work__cardImg" variant="top" src={closet} />
+                    <Card.Body>
+                        <Card.Title className="work__cardTitle">Closet Organizer</Card.Title>
+                        <Card.Text className="work__cardDesc">
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button className="work__cardBtn" variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+                <Card className="work__card" >
+                    <Card.Img className="work__cardImg" variant="top" src={closet} />
+                    <Card.Body>
+                        <Card.Title className="work__cardTitle">Closet Organizer</Card.Title>
+                        <Card.Text className="work__cardDesc">
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button className="work__cardBtn" variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+                {/* style={{ width: '18rem' }} */}
+                <Card className="work__card" >
+                    <Card.Img className="work__cardImg" variant="top" src={closet} />
+                    <Card.Body>
+                        <Card.Title className="work__cardTitle">Closet Organizer</Card.Title>
+                        <Card.Text className="work__cardDesc">
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button className="work__cardBtn" variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+            
         </div>
     )
 }
