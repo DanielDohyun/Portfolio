bb203dd756600eadfadbd7f3a4427871402162c1
Daniel Kim <kkdh1109@gmail.com>
2021-01-06 17:44:59 -0500

    add hovering effect on cards

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
 import './Work.scss';
-// import closet from '../../../public/imgs/closet.png';
 import closet from '../../imgs/closet.png';
-import { Button } from '@material-ui/core';
 
 function Work() {
     return (
@@ -11,96 +8,92 @@ function Work() {
             <h1>My work</h1>
             <h3>Projects</h3>
             <div className="work__categories">
-                <button className="work__btn">All</button>
+                <button className="work__btn active">All</button>
                 <button className="work__btn">Full Stack</button>
                 <button className="work__btn">Back-end</button>
             </div>
-            <div className="work__react">
-                <Card className="work__card" >
-                    <Card.Img className="work__cardImg" variant="top" src={closet} />
-                    <Card.Body>
-                        <Card.Title className="work__cardTitle">Closet Organizer</Card.Title>
-                        <Card.Text className="work__cardDesc">
-                            ●Tools used: HTML, CSS, JavaScript, React, Firebase
-                        </Card.Text>
-                        <Card.Text className="work__cardDesc">
-                        ●Created an app that allows users to organize their closets virtually and have a grasp on all their clothing much faster in a highly time-efficient manner.
-                        </Card.Text>
-                        <Button className="work__cardBtn" variant="outlined" color="primary">
-                            <a className="work__link" target="_blank" href="https://stylish-closet.firebaseapp.com/">
-                                Explore the app
-                            </a>
-                        </Button>
-                    </Card.Body>
-                </Card>
-                <Card className="work__card" >
-                    <Card.Img className="work__cardImg" variant="top" src={closet} />
-                    <Card.Body>
-                        <Card.Title className="work__cardTitle">Closet Organizer</Card.Title>
-                        <Card.Text className="work__cardDesc">
-                            ●Tools used: HTML, CSS, JavaScript, React, Firebase
-                        </Card.Text>
-                        <Card.Text className="work__cardDesc">
-                        ●Created an app that allows users to organize their closets virtually and have a grasp on all their clothing much faster in a highly time-efficient manner.
-                        </Card.Text>
-                        <Button className="work__cardBtn" variant="outlined" color="primary">
-                            <a className="work__link" target="_blank" href="https://stylish-closet.firebaseapp.com/">
-                                Explore the app
-                            </a>
-                        </Button>
-                    </Card.Body>
-                </Card>
-                <Card className="work__card" >
-                    <Card.Img className="work__cardImg" variant="top" src={closet} />
-                    <Card.Body>
-                        <Card.Title className="work__cardTitle">Closet Organizer</Card.Title>
-                        <Card.Text className="work__cardDesc">
-                            ●Tools used: HTML, CSS, JavaScript, React, Firebase
-                        </Card.Text>
-                        <Card.Text className="work__cardDesc">
-                        ●Created an app that allows users to organize their closets virtually and have a grasp on all their clothing much faster in a highly time-efficient manner.
-                        </Card.Text>
-                        <Button className="work__cardBtn" variant="outlined" color="primary">
-                            <a className="work__link" target="_blank" href="https://stylish-closet.firebaseapp.com/">
-                                Explore the app
-                            </a>
-                        </Button>
-                    </Card.Body>
-                </Card>
-                <Card className="work__card" >
-                    <Card.Img className="work__cardImg" variant="top" src={closet} />
-                    <Card.Body>
-                        <Card.Title className="work__cardTitle">Closet Organizer</Card.Title>
-                        <Card.Text className="work__cardDesc">
-                            ●Tools used: HTML, CSS, JavaScript, React, Firebase
-                        </Card.Text>
-                        <Card.Text className="work__cardDesc">
-                        ●Created an app that allows users to organize their closets virtually and have a grasp on all their clothing much faster in a highly time-efficient manner.
-                        </Card.Text>
-                        <Button className="work__cardBtn" variant="outlined" color="primary">
-                            <a className="work__link" target="_blank" href="https://stylish-closet.firebaseapp.com/">
-                                Explore the app
-                            </a>
-                        </Button>
-                    </Card.Body>
-                </Card>
-                <Card className="work__card" >
-                    <Card.Img className="work__cardImg" variant="top" src={closet} />
-                    <Card.Body>
-                        <Card.Title className="work__cardTitle">Closet Organizer</Card.Title>
-                        <Card.Text className="work__cardDesc">
-                            ●Tools used: HTML, CSS, JavaScript, React, Firebase
-                        </Card.Text>
-                        <Card.Text className="work__cardDesc">
-                        ●Created an app that allows users to organize their closets virtually and have a grasp on all their clothing much faster in a highly time-efficient manner.
-                        </Card.Text>
-                        <Button className="work__cardBtn" variant="outlined" color="primary">
-                            <a className="work__link" target="_blank" href="https://stylish-closet.firebaseapp.com/">
-                                Explore the app
-                            </a>
-                        </Button>
-                    </Card.Body>
-                </Card>
+
+            <div className="work__projects">
+                <a
+                    target="_blank"
+                    href="https://stylish-closet.firebaseapp.com/"
+                    className="work__project"
+                >
+                    <img
+                        src={closet}
+                        alt="closet"
+                        className="work__img"
+                    />
+                    <div className="work__desc">
+                        <h3>Closet Organizer</h3>
+                        <span>●Tools used: HTML, CSS, JavaScript, React, Firebase</span>
+                        <span>●Created an app that allows users to organize their closets virtually and have a grasp on all their clothing much faster in a highly time-efficient manner.</span>
+                    </div>
+                </a>
+                <a
+                    target="_blank"
+                    href="https://stylish-closet.firebaseapp.com/"
+                    className="work__project"
+                >
+                    <img
+                        src={closet}
+                        alt="closet"
+                        className="work__img"
+                    />
+                    <div className="work__desc">
+                        <h3>Closet Organizer</h3>
+                        <span>●Tools used: HTML, CSS, JavaScript, React, Firebase</span>
+                        <span>●Created an app that allows users to organize their closets virtually and have a grasp on all their clothing much faster in a highly time-efficient manner.</span>
+                    </div>
+                </a>
+                <a
+                    target="_blank"
+                    href="https://stylish-closet.firebaseapp.com/"
+                    className="work__project"
+                >
+                    <img
+                        src={closet}
+                        alt="closet"
+                        className="work__img"
+                    />
+                    <div className="work__desc">
+                        <h3>Closet Organizer</h3>
+                        <span>●Tools used: HTML, CSS, JavaScript, React, Firebase</span>
+                        <span>●Created an app that allows users to organize their closets virtually and have a grasp on all their clothing much faster in a highly time-efficient manner.</span>
+                    </div>
+                </a>
+                <a
+                    target="_blank"
+                    href="https://stylish-closet.firebaseapp.com/"
+                    className="work__project"
+                >
+                    <img
+                        src={closet}
+                        alt="closet"
+                        className="work__img"
+                    />
+                    <div className="work__desc">
+                        <h3>Closet Organizer</h3>
+                        <span>●Tools used: HTML, CSS, JavaScript, React, Firebase</span>
+                        <span>●Created an app that allows users to organize their closets virtually and have a grasp on all their clothing much faster in a highly time-efficient manner.</span>
+                    </div>
+                </a>
+                <a
+                    target="_blank"
+                    href="https://stylish-closet.firebaseapp.com/"
+                    className="work__project"
+                >
+                    <img
+                        src={closet}
+                        alt="closet"
+                        className="work__img"
+                    />
+                    <div className="work__desc">
+                        <h3>Closet Organizer</h3>
+                        <span>●Tools used: HTML, CSS, JavaScript, React, Firebase</span>
+                        <span>●Created an app that allows users to organize their closets virtually and have a grasp on all their clothing much faster in a highly time-efficient manner.</span>
+                    </div>
+                </a>
                 
             </div>
         </div>
