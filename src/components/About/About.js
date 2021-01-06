18c6ca0b25e73bbe0d28ca00b92751dc0241f710
Daniel Kim <kkdh1109@gmail.com>
2021-01-06 13:15:18 -0500

    add fontawesome script tag

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,25 @@ import './About.css';
 function About() {
     return (
         <div id="about" className="about">
-            
+            <h1>About me</h1>
+            <p>
+                I'm an optimistic, eager to learn, and passionate junior full-stack developer looking for a full-time job where I can grow. I'm continuously developing through cloning the react apps and learning new languages. Stay tuned for my recent works work. Thank you!
+            </p>
+
+            <div className="about__majors">
+                <div className="about__major">
+                    <div className="about__icon">
+                        <i class="fab fa-html5"></i>
+                    
+                    </div>
+                    <div className="about__title">
+                        Front-end
+                    </div>
+                    <div className="about__desc">
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
