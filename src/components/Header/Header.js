186845e3b7784a911a058d4f7941a0164699538a
Daniel Kim <kkdh1109@gmail.com>
2021-01-06 17:58:05 -0500

    add hamburger icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,10 @@ import './Header.css';
 function Header() {
     return (
         <div className="header">
-            
             <h1><Link to="/">Daniel Kim</Link></h1>
+
             <div className="header__right">
-                <ul>
+                <ul className="header__right">
                     <li>
                         <Link className="header__item active" to="/">WORK</Link>
                     </li>
@@ -19,7 +19,12 @@ function Header() {
                         <Link className="header__item" to="/contact">CONTACT</Link>
                     </li>
                 </ul>
+                
             </div>
+
+            <button className="header__toggle">
+                <i class="fas fa-bars"></i>
+            </button>
         </div>
     )
 }
