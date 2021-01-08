95f22d5c70ca672ddd3ad739d467574c301bfc0f
Daniel Kim <kkdh1109@gmail.com>
2021-01-08 15:22:47 -0500

    add msg sent success box

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import db from '../../firebase';
 import './Contact.scss';
 import firebase from 'firebase'
+import CloseIcon from '@material-ui/icons/Close';
 
 function Contact() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [msg, setMsg] = useState('');
+    const [open, setOpen] = useState(false);
 
     const handleSubmit = e => {
         e.preventDefault();
@@ -22,7 +24,12 @@ function Contact() {
         setName('');
         setEmail('');
         setMsg('');
+        setOpen(true);
     };
+
+    const handleClose = () => {
+        setOpen(false);
+    }
 
     return (
         <div className="contact">
@@ -32,11 +39,21 @@ function Contact() {
 
             <form className="contact__form">
                 <div className="contact__inputBox">
-                    <input placeholder="Name" type="text" className="contact__input" name="name" value={name} onChange={e => setName(e.target.value)} />
+                    <input placeholder="Name" type="text" className="contact__input" name="name" value={name} onChange={e => setName(e.target.value)} required/>
 
-                    <input placeholder="Enter email" type="text" className="contact__input" name="email" value={email} onChange={e => setEmail(e.target.value)} />
+                    <input placeholder="Enter email" type="email" className="contact__input" name="email" value={email} onChange={e => setEmail(e.target.value)} required/>
                     
-                    <textarea placeholder="Your Message" className="contact__input" name="msg" id="msg" cols="30" rows="10" value={msg} onChange={e => setMsg(e.target.value)} ></textarea>
+                    <textarea placeholder="Your Message" className="contact__input" name="msg" id="msg" cols="30" rows="10" value={msg} onChange={e => setMsg(e.target.value)} required></textarea>
+                    
+                    <div
+                        className={open ? 'contact__success-open': 'contact__success'}
+                    >
+                            Message sent. Thanks!
+                            <span className="contact__closeBox">
+                                <CloseIcon onClick={handleClose} className="contact__close" />                            
+                            </span>
+                         
+                    </div>
                 </div>
                 <button type="submit" onClick={handleSubmit} className="contact__btn">Submit</button>
             </form>
